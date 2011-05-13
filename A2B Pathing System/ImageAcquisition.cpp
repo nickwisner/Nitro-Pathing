6966b2a65e1079a8278fb1fc33f156f76e19d105
Mastion <mastion11@gmail.com>
2011-05-12 17:56:40 -0700

    nothign changed?

--- A2B Pathing System/ImageAcquisition.cpp
+++ A2B Pathing System/ImageAcquisition.cpp
@@ -50,27 +50,29 @@ ImageAcquisition::~ImageAcquisition()
 }
 void ImageAcquisition::getImages()
 {
+	Mat temp;
 	while(1)
 	{
 		//boost::this_thread::disable_interruption di;
 		// getImage news the image, but it also deletes its current image which is same address as m_plainImage here
-		//m_plainLock.lock();
-		while(!m_plainLock.try_lock())
-			boost::this_thread::yield();
+		temp = Mat(cvQueryFrame(m_capture)).clone();
+		m_plainLock.lock();
+		//while(!m_plainLock.try_lock())
+		//	boost::this_thread::yield();
 		/*m_cap >>*/ m_plainCur = Mat(cvQueryFrame(m_capture)).clone();
 		m_plainLock.unlock();
 
-		while(!m_edgeLock.try_lock())
-			boost::this_thread::yield();
-		//m_edgeLock.lock();
+		//while(!m_edgeLock.try_lock())
+		//	boost::this_thread::yield();
+		m_edgeLock.lock();
 		 // createEdgedImage news the image but has no image to delete so we do it here
 		m_edgeCur = Mat(ImageProcessor::createEdgedImage(&m_plainCur).clone());
 		m_edgeLock.unlock();
 
 
-		while(!m_obstLock.try_lock())
-			boost::this_thread::yield();
-		//m_obstLock.lock();
+		//while(!m_obstLock.try_lock())
+		//	boost::this_thread::yield();
+		m_obstLock.lock();
 		ImageProcessor::mapObstacles(m_edgeCur, m_obstMap);
 		m_obstLock.unlock();
 		
@@ -81,28 +83,28 @@ void ImageAcquisition::getImages()
 }
 Mat ImageAcquisition::getPlain()
 {
-	while(!m_plainLock.try_lock())
-		boost::this_thread::yield();
+	//while(!m_plainLock.try_lock())
+	//	boost::this_thread::yield();
 
-	//m_plainLock.lock();
+	m_plainLock.lock();
 	Mat t = m_plainCur.clone();
 	m_plainLock.unlock();
 	return t;
 }
 Mat ImageAcquisition::getEdge()
 {
-	while(!m_edgeLock.try_lock())
-		boost::this_thread::yield();
-	//m_edgeLock.lock();
+	//while(!m_edgeLock.try_lock())
+	//	boost::this_thread::yield();
+	m_edgeLock.lock();
 	Mat t = m_edgeCur.clone();
 	m_edgeLock.unlock();
 	return t;
 }
 bool* ImageAcquisition::getObstMap()
 {//this is sad-face
-	while(!m_obstLock.try_lock())
-		boost::this_thread::yield();
-	//m_obstLock.lock();
+	//while(!m_obstLock.try_lock())
+	//	boost::this_thread::yield();
+	m_obstLock.lock();
 	bool * tmp = new bool[ROW_SIZE * COL_SIZE];
 	memcpy(tmp, m_obstMap, sizeof(bool)* (ROW_SIZE*COL_SIZE));
 	m_obstLock.unlock();
